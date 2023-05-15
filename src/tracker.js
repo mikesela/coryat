983a9ed1d88e5510afa420c2f4766171f27b43a6
mikesela <mikesela@gmail.com>
2023-05-15 09:27:28 -0700

    styling fixes

--- src/tracker.js
+++ src/tracker.js
@@ -16,10 +16,10 @@ const JeopardyScoreTracker = () => {
 
   return (
     <div className="container">
-      <div className="title">
+      <header className="header">
         <h1>Jeopardy! Coryat Score Tracker</h1>
-        <div className="score">{score}</div>
-      </div>
+        <div className="total-score">Total Score: ${score}</div>
+      </header>
       <div className="content">
         <div className="column">
           <h2>Jeopardy! Round</h2>
@@ -47,65 +47,89 @@ const JeopardyScoreTracker = () => {
         </div>
       </div>
       <style jsx>{`
-        .container {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          text-align: center;
-        }
-        .title {
-          margin-bottom: 20px;
-        }
-        .score {
-          font-size: 36px;
-          margin-top: 10px;
-        }
-        .content {
-          display: flex;
-          justify-content: space-between;
-          width: 100%;
-        }
-        .column {
-          flex: 1;
-          padding: 20px;
-        }
-        .amount-column {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        }
-        .amount-row {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          margin: 10px;
-        }
-        .amount-label {
-          font-size: 24px;
-          margin-right: 10px;
-        }
-        .correct-button {
-          padding: 10px 20px;
-          font-size: 18px;
-          margin: 0 5px;
-          background-color: lightgreen;
-        }
-        .incorrect-button {
-          padding: 10px 20px;
-          font-size: 18px;
-          margin: 0 5px;
-          background-color: lightcoral;
-        }
-
-        @media screen and (max-width: 768px) {
-          .content {
-            flex-direction: column;
-          }
-          .column {
-            margin-top: 20px;
-          }
-        }
-      `}</style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+  
+  .header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: #f0f0f0;
+    padding: 20px;
+    text-align: center;
+    z-index: 999;
+  }
+  
+  .total-score {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+  
+  .content {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-top: 100px; /* Add margin to push content below the fixed header */
+  }
+  
+  .column {
+    flex: 1;
+    padding: 20px;
+    padding-top: 40px; /* Add additional padding at the top of the columns */
+  }
+  
+  .amount-column {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .amount-row {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+    white-space: nowrap; /* Prevent wrapping of the buttons */
+  }
+  
+  .amount-label {
+    font-size: 24px;
+    margin-right: 10px;
+  }
+  
+  .correct-button {
+    padding: 10px 20px;
+    font-size: 18px;
+    margin: 0 5px;
+    background-color: lightgreen;
+  }
+  
+  .incorrect-button {
+    padding: 10px 20px;
+    font-size: 18px;
+    margin: 0 5px;
+    background-color: lightcoral;
+  }
+  
+  @media screen and (max-width: 768px) {
+    .content {
+      flex-direction: column;
+    }
+    .column {
+      margin-top: 20px;
+    }
+  }
+  
+  
+  
+  
+  
+          `}</style>
     </div>
   );
 };
